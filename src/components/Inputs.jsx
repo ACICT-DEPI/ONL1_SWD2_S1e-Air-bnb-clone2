52fc1d76cc032fa528e2057f52eeeb2f5544c94a
bainamagdy <bainamagdy8@gmail.com>
2024-10-14 23:21:04 +0300

    login_Reg

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,44 +1,42 @@
+const Inputs = ({ handleChange, data }) => {
+    return (
+        <>
+            <div className=" border-b border-black pb-2 relative w-full">
+                <input
+                    type="text"
+                    name="email"
+                    id=""
+                    placeholder=" "
+                    className="outline-none bg-transparent w-full z-50  relative peer"
+                    onChange={handleChange}
+                    value={data.email}
+                />
+                <label
+                    htmlFor="email"
+                    className="text-black absolute right-0 h-full z-10 duration-300 -translate-y-6 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6 "
+                >
+                    البريد الالكتروني
+                </label>
+            </div>
+            <div className="w-full border-b border-black pb-2 relative">
+                <input
+                    type="password"
+                    name="password"
+                    id=""
+                    placeholder=" "
+                    className="outline-none bg-transparent w-full z-50  relative peer"
+                    onChange={handleChange}
+                    value={data.password}
+                />
+                <label
+                    htmlFor="password"
+                    className="text-black absolute right-0 h-full z-10 duration-300 -translate-y-6 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6 "
+                >
+                    كلمة السر
+                </label>
+            </div>
+        </>
+    )
+}
 
-
-
-const Inputs=()=>
-    {
-        return(
-            <form className="flex flex-col  gap-11 lg:pl-20 " action="">
-                            <div className="w-[100%] border-b border-black pb-2 relative">
-                                <input
-                                    type="text"
-                                    name="email"
-                                    id=""
-                                    placeholder=" "
-                                    className="outline-none bg-transparent w-full z-50  relative peer"
-                                />
-                                <label
-                                    htmlFor="email"
-                                    className="text-black absolute right-0 h-full z-10 duration-300 -translate-y-6 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6 "
-                                >
-                                    البريد الالكتروني
-                                </label>
-                            </div>
-                            <div className="w-[100%] border-b border-black pb-2 relative">
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id=""
-                                    placeholder=" "
-                                    className="outline-none bg-transparent w-full z-50  relative peer"
-                                />
-                                <label
-                                    htmlFor="password"
-                                    className="text-black absolute right-0 h-full z-10 duration-300 -translate-y-6 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6 "
-                                >
-                                    كلمة السر
-                                </label>
-                            </div>
-                        </form>
-        )
-    
-    }
-    
-    
-    export default Inputs
+export default Inputs
